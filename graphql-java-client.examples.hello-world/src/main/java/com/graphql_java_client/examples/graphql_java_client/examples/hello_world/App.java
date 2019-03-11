321f042c72c38dff2239cfab3bc334d360bb1b1e
VJ2053 <VJ2053@UPL60002525.sdmn01.sirius.infra.com>
2019-03-11 22:16:32 +0100

    First commit, non usable, as a backup

---
+++ graphql-java-client.examples.hello-world/src/main/java/com/graphql_java_client/examples/graphql_java_client/examples/hello_world/App.java
@@ -0,0 +1,13 @@
+package com.graphql_java_client.examples.graphql_java_client.examples.hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ graphql-java-client.examples.hello-world/src/main/java/com/graphql_java_client/examples/graphql_java_client/examples/hello_world/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the object to map as input parameters for queries (mutations...) and for responses, when
+ * querying the GraphQL server
+ * 
+ * @author EtienneSF
+ */
+package com.graphql_java_client.examples.graphql_java_client.examples.hello_world.domain;
